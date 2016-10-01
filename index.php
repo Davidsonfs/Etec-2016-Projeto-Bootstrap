0d5c32be09629fc5324704d42de57fab09ef34ad
Davidson ferreira da Silva <webestilos@hotmail.com>
2016-10-01 17:40:27 -0300

    Index do Site

---
+++ index.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Meu CSS -->
+    <link href="css/style.css" rel="stylesheet">
+ 
+  </head>
+  <body>
+  <div class="container">
+
+    <header>
+    	<h1></span><a href="">Etec 2016 ...<span class="glyphicon glyphicon-pencil"></a></h1>
+    	<p><span class="glyphicon glyphicon-blackboard"></span> Workshop sobre o Twitter Bootstrap 
+    	<span class="pull-right">Data: 21/09/2016</span></p>
+    </header>
+
+    <nav class="navbar navbar-default">
+	    <ul class="nav navbar-nav">
+	      <li class="active"><a href="">Home</a></li>
+	      <li><a href="">Sobre</a></li>
+	      <li><a href="">Conteúdo</a></li>
+	      <li><a href="">Convidado</a></li>
+	    </ul>
+    </nav>
+
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+	  <!-- Indicators -->
+	  <ol class="carousel-indicators">
+	    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+
+	  </ol>
+
+	  <!-- Wrapper for slides -->
+	  <div class="carousel-inner" role="listbox">
+	  <div class="item active">
+	      <img src="img/8.jpg" alt="...">
+	      <div class="carousel-caption">
+	       Etec 2016 - Workshop sobre o Twitter Bootstrap
+	      </div>
+	    </div>
+	    <div class="item">
+	      <img src="img/1.jpg" alt="...">
+	      <div class="carousel-caption">
+	        Aprenda a Utilizar o Twitter Boostrap de uma vez por todas...
+	      </div>
+	    </div>
+	    <div class="item">
+	      <img src="img/5.jpg" alt="...">
+	      <div class="carousel-caption">
+	        Crie Sites Elegantes com o Boostrap framework
+	      </div>
+	    </div>
+	  
+	  </div>
+
+	  <!-- Controls -->
+	  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+	    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+
+	  </a>
+	  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+	    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+	 
+	  </a>
+	</div>
+	<section id="sobre">
+		<hr>
+		<h2 class="text-center text-primary">Sobre o Workshop</h2>
+	  	<p class="text-center">Semana tenologica - Informática para intente 2016</p>
+	  	<hr>
+		<div class="row">
+
+			<div class="col-lg-4">
+				<a href="#">
+					<img class="img-thumbnail" src="img/Chegando na Etec-caraguá 3.JPG" alt="..." class="img-responsive">
+				</a>
+			</div>
+			<div class="col-lg-8">
+				<h3 class="text-danger">Organizador Carlos Chagas</h3>
+					    <p>Wokshop organizado pelo professor de TI Carlos chgas. Elit, sed do eiusmod
+					    tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					    tempor incididunt.</p>
+
+					    <p>consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+					    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					    tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					    tempor incididunt ut labore et dolore magna aliqua.</p>
+			</div>
+		</div>
+			
+		
+	</section>
+	<section id="conteudo">
+		<hr>	
+	  	<h2 class="text-center text-primary">Conteúdo Programado</h2>
+	  	<p class="text-center">Conheça os nosso serviços e produtos de alta qualidade</p>
+	  	<hr>
+
+	  	<div class="row">
+		  		<div class="col-lg-4">
+		  			<div class="media">
+					  <div class="media-left">
+					    <a href="#" class="badge">
+					      <h1>A</h1>
+					    </a>
+					  </div>
+					  <div class="media-body">
+					    <h4 class="media-heading text-danger">Apresentação</h4>
+					    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					    tempor.
+					  </div>
+					</div>
+		  		</div>
+		  		<div class="col-lg-4">
+		  			<div class="media">
+					  <div class="media-left">
+					    <a href="#" class="badge">
+					      <h1>S</h1>
+					    </a>
+					  </div>
+					  <div class="media-body">
+					    <h4 class="media-heading text-danger">Sistema de Grid</h4>
+					    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					    tempor.
+					  </div>
+					</div>
+		  		</div>
+		  		<div class="col-lg-4">
+		  			<div class="media">
+					  <div class="media-left">
+					    <a href="#" class="badge">
+					      <h1>T</h1>
+					    </a>
+					  </div>
+					  <div class="media-body">
+					    <h4 class="media-heading text-danger">Tipográfia</h4>
+					    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					    tempor.
+					  </div>
+					</div>
+		  		</div>
+	  		
+	  		</div>
+	  		<br><br>
+	  		<div class="row">
+		  		<div class="col-lg-4">
+		  			<div class="media">
+					  <div class="media-left">
+					    <a href="#" class="badge">
+					      <h1>F</h1>
+					    </a>
+					  </div>
+					  <div class="media-body">
+					    <h4 class="media-heading text-danger">Formulários</h4>
+					    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					    tempor.
+					  </div>
+					</div>
+		  		</div>
+		  		<div class="col-lg-4">
+		  			<div class="media">
+					  <div class="media-left">
+					    <a href="#" class="badge">
+					      <h1>B</h1>
+					    </a>
+					  </div>
+					  <div class="media-body">
+					    <h4 class="media-heading text-danger">Botões</h4>
+					    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					    tempor.
+					  </div>
+					</div>
+		  		</div>
+		  		<div class="col-lg-4">
+		  			<div class="media">
+					  <div class="media-left">
+					    <a href="#" class="badge">
+					      <h1>C</h1>
+					    </a>
+					  </div>
+					  <div class="media-body">
+					    <h4 class="media-heading text-danger">Classes auxiliares</h4>
+					    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					    tempor.</p>
+					  </div>
+					</div>
+		  		</div>
+	  		
+	  		</div>
+	  		<br/>
+	  		</section>
+	  		<div class="well text-center">
+	  		<h2 class="text-center text-primary">Convidado do Dia <span class="glyphicon glyphicon-user" aria-hidden="true"></span></h2>
+			  	<p class="text-center text-danger">Convidado do dia para o Workshop de Boostrap & PHP</p>
+			  	<hr>
+	  			<img src="img/12.jpg" alt="..." class="img-thumbnail" width="500px">
+	  			<h4 class="text-danger">Davidson Ferreira da Silva .</h4>
+	  			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	  			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	  			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	  			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	  			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	  			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	  		</div>
+
+	  	</div>
+	  	<hr>
+	  	<footer class="">
+	  		<p class="text-center"><b>Todos os Direitos Reservados &copy; Etec 2016</b></p>
+	  	</footer>
+	 
+
+ </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
